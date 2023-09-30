d31a2cbe71b7b6facf186f8ceb55ff2b6925bd9c
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-09-30 15:51:09 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import React, { useState } from "react";
-import Items from "./components/Items";
-import { ShopContext } from "./components/ShopContext";
-import { itemList } from './components/ItemList';
+
+import Home from "./pages/home/Home";
+
+
 
 function App() {
-
-const [orders, setOrders] = useState([])
-
-function deleteOrder(id){
-  setOrders(orders.filter(el=> el.id !== id ))
-}
-
-function  addToOrder(item){
-  const isDuplicate = orders.find(order => order.id === item.id);
-  if (!isDuplicate) {
-    setOrders([...orders, item])
-  }
-}
-
-const printItems = () => {
-  console.warn(itemList);
-}
-
   return (
-    <ShopContext.Provider value={{
-      deleteOrder, addToOrder, printItems
-    }}>
-      <div className="wrapper">
-        <Header orders={orders} />
-        <Items items={itemList} />
-        <Footer />
-      </div>
-    </ShopContext.Provider>
-  );
+   <Home/>
+  
+  )
 }
 
 export default App;
